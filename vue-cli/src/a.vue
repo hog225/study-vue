ed21efaa658949451cc6cd4c29d8bb6a2b2430d0
yeonggihong <epikhighjin@gmail.com>
2021-09-12 22:43:43 +0900

    Commit error 로 재 커밋

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,29 @@
+
+// new Vue({
+//     template: '<div>header</div>',
+//         methods: {
+//         addNum: function(){
+
+//         }
+//     }
+// })
+
+<template>
+  <!-- html -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    //java script -- 인스턴스 옵션 
+    methods: {
+        addNum: function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+/* css */
+</style>
